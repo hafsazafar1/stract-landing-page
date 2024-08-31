637016e1883e83ece02a93abe99d6055a384691a
Hafsa Zafar <hafsazafar384@gmail.com>
2024-08-31 08:17:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let button  = document.getElementById("toggle-menu");
+console.log(button);
+
+
+button.addEventListener("click", () =>{
+ let navLinks = document.getElementById("nav-links")
+ navLinks.classList.toggle("active")
+}) 
+
